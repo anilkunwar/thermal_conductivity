33ae7580e34761973d6235d43f44900c51ef3af3
Anil Kunwar <anilkunwar318@yahoo.com>
2023-06-26 17:43:36 +0200

    Update anisotropic_conductivity.py

--- anisotropic_conductivity.py
+++ anisotropic_conductivity.py
@@ -58,9 +58,9 @@ Z_scaled = Z * radius_c
 
 # set the colormap and normalization
 # Choose the color map
-cmap_name = st.selectbox("Select a color map", ['flag', 'prism', 'ocean', 'gist_earth', 'terrain',
+cmap_name = st.selectbox("Select a color map", ['jet', 'flag', 'prism', 'ocean', 'gist_earth', 'terrain',
                       'gist_stern', 'gnuplot', 'gnuplot2', 'CMRmap',
-                      'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet',
+                      'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 
                       'turbo', 'nipy_spectral', 'gist_ncar', 'Pastel1', 'Pastel2',
                       'Paired', 'Accent', 'Dark2', 'Set1', 'Set2', 'Set3', 'tab10',
                       'tab20', 'tab20b', 'tab20c', 'twilight', 'twilight_shifted',
